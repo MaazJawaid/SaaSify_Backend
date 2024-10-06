ba6b4287ef65920fa8861cff72a0b0a73b093b1b
Maaz Jawaid <159764144+MaazJawaid@users.noreply.github.com>
2024-10-06 14:53:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(cookieParser())
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Configure CORS
-const allowedOrigin = 'https://chipper-faun-0cd5fd.netlify.app'
+const allowedOrigin = 'https://gorgeous-entremet-6b6c0e.netlify.app'
 app.use(cors({
     origin: allowedOrigin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -817,7 +817,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://chipper-faun-0cd5fd.netlify.app',
+        origin: 'https://gorgeous-entremet-6b6c0e.netlify.app',
         methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
         credentials: true
     }
