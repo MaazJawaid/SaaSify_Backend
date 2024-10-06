0e6a8cd160f58156433ed32f08569c0f975e4aeb
Maaz Jawaid <159764144+MaazJawaid@users.noreply.github.com>
2024-10-06 13:58:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(cookieParser())
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Configure CORS
-const allowedOrigin = 'https://resilient-daifuku-553c3f.netlify.app'
+const allowedOrigin = 'https://thriving-lokum-53e4a6.netlify.app'
 app.use(cors({
     origin: allowedOrigin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -817,7 +817,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://resilient-daifuku-553c3f.netlify.app',
+        origin: 'https://thriving-lokum-53e4a6.netlify.app',
         methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
         credentials: true
     }
