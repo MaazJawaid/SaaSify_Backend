ca4c2d0e6b686b65fb49329b3646facdfbfcf6e2
Maaz Jawaid <159764144+MaazJawaid@users.noreply.github.com>
2024-10-10 11:44:21 +0500

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(cookieParser())
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Configure CORS
-const allowedOrigin = 'https://sprightly-fairy-8dd801.netlify.app'
+const allowedOrigin = 'https://eclectic-rabanadas-4b1ccb.netlify.app'
 app.use(cors({
     origin: allowedOrigin,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -817,7 +817,7 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://sprightly-fairy-8dd801.netlify.app',
+        origin: 'https://eclectic-rabanadas-4b1ccb.netlify.app',
         methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
         credentials: true
     }
